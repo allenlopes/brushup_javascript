3d39f4244491d8eb2f3d1b1051b53dc8f2827497
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-14 22:06:15 +0530

    Anonymous Functions Completedgit add 11_AnonymousFunctions/

---
+++ 11_AnonymousFunctions/1_test.js
@@ -0,0 +1,16 @@
+/*
+function abc(){
+let x = 10;
+let y = 20;
+document.write(x + y);
+}
+abc();
+*/
+
+// Anonymous function
+(function () {
+  let x = 10;
+  let y = 20;
+  document.write(x + y);
+}) (); // () this indicated function is called.
+
---
+++ 11_AnonymousFunctions/2_test.js
@@ -0,0 +1,15 @@
+/*
+function abc() {
+let x = 30;
+let y = 40;
+document.write(x * y);
+}
+abc();
+*/
+
+// Anonymous function
+(function () {
+  let x = 10;
+  let y = 20;
+  document.write(x * y);
+}) ();  // () this indicated function is called.
