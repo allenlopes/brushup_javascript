71741badbacb01da675e2b80ca7ac768ed2973ff
sudoallenlopes23 <allenlopes10@gmail.com>
2023-09-23 23:27:37 +0530

    Methods

--- 57_Project_4/code.js
+++ 57_Project_4/code.js
@@ -14,26 +14,72 @@ let numGuess = 1; // number of guesses starts from 1
 
 let playGame = true; // it allows you to play the game again and again even after the total no. of attempts are done.
 
+// check if user is available to play game
+if(playGame){
+    submit.addEventListener('click', function(evnt){
+        evnt.preventDefault(); // preventDefault() matlab ham evnt ko bolte hai values ko kahi mat leke jao vahi pe rokke rakho.
+        const guess = parseInt(userInput.value)
+        validateGuess(guess);
+    });
+};
 
 function validateGuess(guess){
     /* In this function we check if the user has entered an integer number only, and also if user enters numbers
     between 1 to 100 only, and if user enters an alphabet or any negative numbers. */
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    }
+    else if(guess < 1){
+        alert("Please enter a number greater than 0");
+    }
+    else if(guess > 100){
+        alert("Please enter a number less than 100");
+    }
+    else {
+        prevGuess.push(guess);
+        if(numGuess === 11){
+            displayGuess(guess);
+            displayMessage(`Game Over. Random number was ${randomNumber}`);
+            endGame();
+        }
+        else{
+            displayGuess(guess);
+            checkGuess(guess)
+        }
+    }
 };
+
 
 function checkGuess(guess){
     /* In this function we check if the value guessed by user is equal to random no. or not, if it's equal to random
     number we dispay the message using displayMessage() function and, if it's not equal to tandom number we check here
     if it's low value than the random no. or high value than the random no. */
+    if(guess === randomNumber){
+        displayMessage(`You Guessed it right`)
+        endGame()
+    }
+    else if(guess < randomNumber){
+        displayMessage(`Number is Tooo Low`)
+    }
+    else if(guess > randomNumber){
+        displayMessage(`Number is Too High`)
+    }
 };
 
 
 function displayGuess(guess){
-    /* It will clean the values inside the input box, and also update the array of prevois guesses, and also update
+    /* It will clean the values inside the input box, and also update the array of previous guesses, and also update
     the remaining guesses. */
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}, `;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`;
 };
+
 
 function displayMessage(message){
     // this function will directly interact with DOM manipulation.
+    lowOrHi.innerHTML = `<h2>${message}</h2>`
 };
 
 
